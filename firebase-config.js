bff2250c49f2ea61026a17ffe1cfdd3a1c250ece
kalumsneha <kalumsneha@users.noreply.github.com>
2026-02-12 20:20:30 +0000

    deploy: c170b8ee5a76c416cf18790da7f0742dd0f530b1

---
+++ firebase-config.js
@@ -0,0 +1,7 @@
+window.FIREBASE_API_KEY='AIzaSyD963zVhunlASDiItt4VpDfazRyam7_MJU';
+window.FIREBASE_AUTH_DOMAIN='waiting-list-manager.firebaseapp.com';
+window.FIREBASE_DB_URL='https://waiting-list-manager-default-rtdb.firebaseio.com';
+window.FIREBASE_PROJECT_ID='waiting-list-manager';
+window.FIREBASE_STORAGE_BUCKET='waiting-list-manager.firebasestorage.app';
+window.FIREBASE_MESSAGING_SENDER_ID='286480324159';
+window.FIREBASE_APP_ID='1:286480324159:web:f184b96aab8ccff19b0a8a';
