5f7875147829998e608fd0df0868245e7d37a35c
kalumsneha <kalumsneha@users.noreply.github.com>
2026-02-12 20:38:45 +0000

    deploy: 2691770fe97359f43b8cef0bb624e4983e2c6a93

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyD963zVhunlASDiItt4VpDfazRyam7_MJU',
+  authDomain: 'waiting-list-manager.firebaseapp.com',
+  databaseURL: 'https://waiting-list-manager-default-rtdb.firebaseio.com',
+  projectId: 'waiting-list-manager',
+  storageBucket: 'waiting-list-manager.firebasestorage.app',
+  messagingSenderId: '286480324159',
+  appId: '1:286480324159:web:f184b96aab8ccff19b0a8a'
+};
